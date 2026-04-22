f22167d3310550a658f1db506333455a64a0dc3e
k-ankush07 <ankush@ecombithub.com>
2026-04-23 05:05:13 +0530

    chnages done

--- src/Components/Next_Project.jsx
+++ src/Components/Next_Project.jsx
@@ -8,7 +8,7 @@ function Next_Project() {
    
             <div className='relative bg-[#000000] w-full lg:w-[60%]'>
         <img src={Blur} alt=' blur logo'  className='absolute right-0'/>
-        <div className=' border  border-[#292929] px-6 pt-[53px] pb-[46px] sm:px-[108px] rounded-md flex flex-col gap-6'>
+        <div className=' border  border-[#292929] px-6 pt-[53px] pb-[54px] sm:px-[108px] rounded-md flex flex-col gap-6'>
             <div className='text-center flex flex-col gap-2  '>
             <h1 className='font-bold text-[24px] text-[#FFFFFF]'>Let's work together on your next project</h1>
             <p className='text-[16px]  text-[#BDBDBD]'>Open communication, async updates, zero surprises</p>
@@ -17,7 +17,7 @@ function Next_Project() {
             <img src={users} alt='users' className='' />
         </div>
         <div className='relative'>
-            <img src={arrow} alt='arrow' className='absolute -right-2  lg:-right-22' />
+            <img src={arrow} alt='arrow' className='absolute -right-2  top-1 lg:-right-22' />
         </div>
         </div>
     </div>
