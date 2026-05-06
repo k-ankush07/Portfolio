bfaf86df0e35c8079f2d7b96c7908e409b3b1e55
k-ankush07 <ankush@ecombithub.com>
2026-05-06 21:31:33 +0530

    git

--- src/Components/AboutPage/GithubStats.jsx
+++ src/Components/AboutPage/GithubStats.jsx
@@ -26,22 +26,22 @@ function GithubStats({ githubData = {} }) {
 
   return (
     <div className="mt-[10px] ">
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-[10px] lg:pb-[40px] relative">
+      <div className="grid grid-cols-3 gap-[10px] lg:pb-[40px] relative">
         {stats.map((item, index) => (
           <div
             key={index}
-            className="relative border border-[#292929] rounded-[8px] lg:px-[39px] lg:py-[44px] p-5 overflow-hidden"
+            className="relative border  border-[#292929] rounded-[8px]  lg:px-[39px] lg:py-[44px]  overflow-hidden"
           >
             <div className="absolute right-0 top-0 overflow-hidden rounded-[8px]">
               <img src={gitbottomblur} alt="blur image" />
             </div>
 
-            <div className="pt-[15px] pl-[20px] pb-[20px] lg:pt-[0px] lg:pr-[0px] lg:pb-[0px] flex flex-col lg:flex-row lg:justify-between lg:items-center z-10">
-              <p className="text-[#BDBDBD] text-[14px]">
+            <div className="pt-[15px] pl-[10px] pb-[20px] sm:pl-[20px] lg:pt-[0px] lg:pr-[0px] lg:pb-[0px] flex flex-col lg:flex-row lg:justify-between lg:items-center z-10">
+              <p className="text-[#BDBDBD] text-[12px] lg:text-[14px]">
                 {item.label}
               </p>
 
-              <h2 className={`text-[32px] font-bold ${item.color}`}>
+              <h2 className={` text-[30px] lg:text-[32px] font-bold ${item.color}`}>
                 {item.value}
               </h2>
             </div>
