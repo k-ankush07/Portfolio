21aa26c1150b8ba9616c2ab34bdd14c5122b8715
k-ankush07 <ankush@ecombithub.com>
2026-05-12 21:00:04 +0530

    start

--- README.md
+++ README.md
@@ -26,3 +26,5 @@ If you are developing a production application, we recommend using TypeScript wi
 
 
 
+
+
--- src/Components/BlogPage/Typography.jsx
+++ src/Components/BlogPage/Typography.jsx
@@ -1,37 +1,3 @@
-// import React from "react";
-
-// export const Breadcrumb = ({ children }) => (
-//   <p className="text-[#BDBDBD] text-center text-[12px] lg:text-[14.4px] leading-[15px] lg:leading-[21.6px]  ">
-//     {children}
-//   </p>
-// );
-
-// export const Title = ({ children }) => (
-//   <h1 className="text-[18px] lg:text-[44px] leading-[50px] lg:leading-[76.8px] font-bold text-white">
-//     {children}
-//   </h1>
-// );
-
-// export const Subtitle = ({ children }) => (
-//   <p className=" text-[12px] lg:text-[18px] leading-[19px]  lg:leading-[27px] text-[#FFFFFF] ">
-//     {children}
-//   </p>
-// );
-
-// export const Heading = ({ children }) => (
-//   <h2 className="text-[18px] lg:text-[24px] leading-[25px] lg:leading-[33px] font-bold text-white">
-//     {children}
-//   </h2>
-// );
-
-// export const Paragraph = ({ children, className = "" }) => (
-//   <p className={` text-[12px] lg:text-[16px] leading-[19px] lg:leading-[24px] text-[#BDBDBD]  ${className}`}>
-//     {children}
-//   </p>
-// );
-
-
-
 import React from "react";
 import { motion } from "framer-motion";
 
