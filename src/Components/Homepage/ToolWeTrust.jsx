2580364d416e89367e67540c1226c454c2898e10
k-ankush07 <ankush@ecombithub.com>
2026-05-26 00:05:19 +0530

    slider touct issue

--- src/Components/Homepage/ToolWeTrust.jsx
+++ src/Components/Homepage/ToolWeTrust.jsx
@@ -33,6 +33,8 @@ function ToolWeTrust() {
 
     return (
         <div className='about_class flex w-full xl:w-[33.3%] mb-0 cursor-pointer'
+            onTouchStart={() => setIsHovered(true)}
+            onTouchEnd={() => setIsHovered(false)}
             onMouseEnter={() => setIsHovered(true)}
             onMouseLeave={() => setIsHovered(false)}
         >
