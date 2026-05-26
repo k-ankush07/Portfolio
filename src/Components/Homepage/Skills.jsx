ea0343d149e3b0472c1d75568d1a8fdf15d73f20
k-ankush07 <ankush@ecombithub.com>
2026-05-27 03:33:18 +0530

    svg images

--- src/Components/Homepage/Skills.jsx
+++ src/Components/Homepage/Skills.jsx
@@ -53,7 +53,7 @@ function Skills() {
                   }}
                   loopAdditionalSlides={10}
                   autoplay={{
-                    delay: 1,
+                    delay: 0,
                     disableOnInteraction: false,
                   }}
                   speed={5000}
@@ -168,7 +168,7 @@ function Skills() {
                   allowTouchMove={false}
                   loopAdditionalSlides={10}
                   autoplay={{
-                    delay: 1,
+                    delay: 0,
                     disableOnInteraction: false,
                   }}
                   dir="rtl"
@@ -366,7 +366,7 @@ function Skills() {
                   }}
                   loopAdditionalSlides={10}
                   autoplay={{
-                    delay: 1,
+                    delay: 0,
                     disableOnInteraction: false,
                   }}
                   speed={5000}
