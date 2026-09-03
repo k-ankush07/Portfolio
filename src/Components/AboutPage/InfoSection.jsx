020b0612d434043915017887df181cec9e84035c
k-ankush07 <ankush@ecombithub.com>
2026-09-03 14:52:57 +0530

    working on about page section

--- src/Components/AboutPage/InfoSection.jsx
+++ src/Components/AboutPage/InfoSection.jsx
@@ -9,12 +9,20 @@ function InfoSection() {
     const logos = [Overlay12, Overlay13, Overlay14, Overlay15];
   return (
     <section className='container'>
-        <div className='px-2 md:px-0  lg:flex '> 
+        <div className='px-2 md:px-0  lg:flex  lg:justify-between gap-[92px] '> 
 
             {/* left section  */}
-            <div>
+            <div className='max-w-[689px] '>
                 <div className='max-w-[329px] '>
-                    <h2 className='text-[40px] leading-[48px] '>I'm Aayush, a <br/>creative <span>engineer</span></h2>
+                    <h2 className='text-[40px] leading-[48px] font-bold '>I'm Aayush, a <br/>creative <span className='bg-gradient-to-r from-[#D76D77] via-[#D76D77] to-[#FFAF7B] bg-clip-text text-transparent'>engineer</span></h2>
+                </div>
+                <div className='pt-[10px] text-[#BDBDBD]'>
+                    <p>I'm Aayush Bharti, a passionate full-stack developer focused on creating modern, responsive, and engaging digital experiences. I enjoy turning ideas into clean, functional websites and applications that feel as good as they work.<br/> <br/>
+From frontend interfaces to backend functionality, I love working across the stack and solving complex problems with thoughtful, efficient code. I'm constantly exploring new technologies, improving my skills, and finding better ways to build for the web.<br/> <br/>
+When I'm not coding, I'm usually exploring new ideas, experimenting with creative concepts, or learning something new. I believe great work comes from staying curious, paying attention to details, and continuously pushing yourself forward.</p>
+<br/> <br/>
+
+<p>I build with curiosity, create with purpose, and always look for what's next.</p>
                 </div>
             </div>
             <div className='flex '>
