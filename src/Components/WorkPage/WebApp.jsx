0240f1c2b09e61700bba193c8302bed7eaea66dd
k-ankush07 <ankush@ecombithub.com>
2026-09-08 10:56:43 +0530

    work on work page

--- src/Components/WorkPage/WebApp.jsx
+++ src/Components/WorkPage/WebApp.jsx
@@ -1,3 +1,145 @@
+// // import React from "react";
+// // import shopify from "../../assets/Images/Vector (5).svg";
+// // import smallicon from "../../assets/Images/smallicon.svg";
+// // import ViewDetail from "../Homepage/ViewDetail";
+// // import new1 from "../../assets/Images/new1 (1).svg";
+// // import new2 from "../../assets/Images/new1 (2).svg";
+// // import new3 from "../../assets/Images/new1 (3).svg";
+// // import new4 from "../../assets/Images/new1 (4).svg";
+// // import new5 from "../../assets/Images/new1 (5).svg";
+// // import new6 from "../../assets/Images/new1 (6).svg";
+// // import ProjectTitle from "./ProjectTitle";
+// // import { Link } from "react-router-dom";
+// // import CardSection from "../Homepage/CardSection";
+
+// // function WebApp({
+// //   single,
+// //   leftImg,
+// //   centerImg,
+// //   rightImg,
+// //   leftImg2,
+// //   centerImg2,
+// //   rightImg2,
+// //   title,
+// //   title2,
+// // }) {
+// //   const logos = [
+// //     new2, new3, new1, new5, new4, new6,
+// //     new1, new2, new3, new4, new5, new6,
+// //   ];
+
+// //   const CARDSECTION_DOUBLE_IDS = [3];
+// //   const CARDSECTION_SINGLE_IDS = [7];
+
+// //   const renderPreview = (id, l, c, r) => {
+// //     if (CARDSECTION_DOUBLE_IDS.includes(id)) {
+// //       return <CardSection frontimg={l} backimg={c} frontRotate={8} backRotate={-6} />;
+// //     }
+// //     if (CARDSECTION_SINGLE_IDS.includes(id)) {
+// //       return <CardSection frontimg={c} />;
+// //     }
+// //     return <ViewDetail leftImg={l} centerImg={c} rightImg={r} />;
+// //   };
+
+// //   return (
+// //     <section className="lato-regular">
+// //       <div className="container">
+// //         <div className="lg:flex ">
+// //           <div className=" relative  pt-[40px] lg:pt-0  lg:border-r-[1px]  lg:border-[#FFFFFF]/10 lg:w-[50%]">
+// //             <div className="lg:pr-[57px] ">
+// //               <div className="  flex justify-between"></div>
+// //               <ProjectTitle logo={shopify} title={title} highlight={highlight} />
+// //             </div>
+// //             <div className=" h-0 lg:h-[28px] flex  items-center">
+// //               <div className=" hidden lg:block h-[1px] w-full  bg-white/10"></div>
+// //             </div>
+// //             <div className="hidden lg:block absolute -right-4 top-19  ">
+// //               <img src={smallicon} alt="icon" />
+// //             </div>
+
+// //             <div className=" pt-[16px] lg:pt-0 lg:pr-[57px]  ">
+// //               <Link to={`/work/${slug}`}>
+// //                 {renderPreview(idA, leftImg, centerImg, rightImg)}
+// //               </Link>
+
+// //               <div className="pt-[20px] lg:pt-[24px]">
+// //                 <div className="flex flex-wrap gap-[6px] sm:gap-[11px]  sm:max-w-[450px] xl:hidden">
+// //                   {logos.slice(0, 8).map((img, i) => (
+// //                     <img key={i} src={img} alt="logo" className="" />
+// //                   ))}
+// //                 </div>
+// //                 <div className="hidden sm:max-w-[610px] xl:flex flex-wrap gap-[11px]">
+// //                   {logos.slice(0, 13).map((img, i) => (
+// //                     <img key={i} src={img} alt="logo" className="" />
+// //                   ))}
+// //                 </div>
+// //                 {tags?.length > 0 && (
+// //                   <div className="flex flex-wrap gap-[8px] pt-[12px]">
+// //                     {tags.map((tag, i) => (
+// //                       <span
+// //                         key={i}
+// //                         className="text-[11px] lg:text-[12px] leading-[16px] text-[#9DB8FF] bg-[#1B2340] border border-[#3E4A7A]/50 px-[10px] py-[5px] rounded-[6px]"
+// //                       >
+// //                         {tag}
+// //                       </span>
+// //                     ))}
+// //                   </div>
+// //                 )}
+// //               </div>
+// //             </div>
+// //           </div>
+
+// //           {!single && (
+// //             <div className=" relative  px-2 lg:px-0 pt-[40px] lg:pt-[211px]  lg:w-[50%]">
+// //               <div className=" lg:pl-[58px]">
+// //                 <ProjectTitle logo={shopify} title={title2} highlight={highlight2} />
+// //               </div>
+
+// //               <div className=" h-0 lg:h-[28px] flex  items-center">
+// //                 <div className="hidden lg:block h-[1px] w-full  bg-white/10"></div>
+// //               </div>
+// //               <div className="hidden lg:block absolute -left-3 top-[300px]  ">
+// //                 <img src={smallicon} alt="icon" />
+// //               </div>
+// //               <div className=" pt-[16px]  lg:pt-0 lg:pl-[57px]">
+// //                 <Link to={`/work/${slug2}`}>
+// //                   {renderPreview(idB, leftImg2, centerImg2, rightImg2)}
+// //                 </Link>
+
+// //                 <div className="pt-[20px] lg:pt-[24px]">
+// //                   <div className="flex flex-wrap gap-[6px] sm:gap-[11px]  sm:max-w-[450px] xl:hidden">
+// //                     {logos.slice(0, 8).map((img, i) => (
+// //                       <img key={i} src={img} alt="logo" className="" />
+// //                     ))}
+// //                   </div>
+// //                   <div className="hidden sm:max-w-[610px] xl:flex flex-wrap gap-[11px]">
+// //                     {logos.slice(0, 13).map((img, i) => (
+// //                       <img key={i} src={img} alt="logo" className="" />
+// //                     ))}
+// //                   </div>
+// //                   {tags2?.length > 0 && (
+// //                     <div className="flex flex-wrap gap-[8px] pt-[12px]">
+// //                       {tags2.map((tag, i) => (
+// //                         <span
+// //                           key={i}
+// //                           className="text-[11px] lg:text-[12px] leading-[16px] text-[#9DB8FF] bg-[#1B2340] border border-[#3E4A7A]/50 px-[10px] py-[5px] rounded-[6px]"
+// //                         >
+// //                           {tag}
+// //                         </span>
+// //                       ))}
+// //                     </div>
+// //                   )}
+// //                 </div>
+// //               </div>
+// //             </div>
+// //           )}
+// //         </div>
+// //       </div>
+// //     </section>
+// //   );
+// // }
+
+// // export default WebApp;
 // import React from "react";
 // import shopify from "../../assets/Images/Vector (5).svg";
 // import smallicon from "../../assets/Images/smallicon.svg";
@@ -10,7 +152,6 @@
 // import new6 from "../../assets/Images/new1 (6).svg";
 // import ProjectTitle from "./ProjectTitle";
 // import { Link } from "react-router-dom";
-// import CardSection from "../Homepage/CardSection";
 
 // function WebApp({
 //   single,
@@ -21,25 +162,13 @@
 //   centerImg2,
 //   rightImg2,
 //   title,
+//   highlight,
 //   title2,
 // }) {
 //   const logos = [
 //     new2, new3, new1, new5, new4, new6,
 //     new1, new2, new3, new4, new5, new6,
 //   ];
-
-//   const CARDSECTION_DOUBLE_IDS = [3];
-//   const CARDSECTION_SINGLE_IDS = [7];
-
-//   const renderPreview = (id, l, c, r) => {
-//     if (CARDSECTION_DOUBLE_IDS.includes(id)) {
-//       return <CardSection frontimg={l} backimg={c} frontRotate={8} backRotate={-6} />;
-//     }
-//     if (CARDSECTION_SINGLE_IDS.includes(id)) {
-//       return <CardSection frontimg={c} />;
-//     }
-//     return <ViewDetail leftImg={l} centerImg={c} rightImg={r} />;
-//   };
 
 //   return (
 //     <section className="lato-regular">
@@ -58,8 +187,8 @@
 //             </div>
 
 //             <div className=" pt-[16px] lg:pt-0 lg:pr-[57px]  ">
-//               <Link to={`/work/${slug}`}>
-//                 {renderPreview(idA, leftImg, centerImg, rightImg)}
+//               <Link to={`/work/${title?.toLowerCase()}`}>
+//                 <ViewDetail leftImg={leftImg} centerImg={centerImg} rightImg={rightImg} />
 //               </Link>
 
 //               <div className="pt-[20px] lg:pt-[24px]">
@@ -73,18 +202,6 @@
 //                     <img key={i} src={img} alt="logo" className="" />
 //                   ))}
 //                 </div>
-//                 {tags?.length > 0 && (
-//                   <div className="flex flex-wrap gap-[8px] pt-[12px]">
-//                     {tags.map((tag, i) => (
-//                       <span
-//                         key={i}
-//                         className="text-[11px] lg:text-[12px] leading-[16px] text-[#9DB8FF] bg-[#1B2340] border border-[#3E4A7A]/50 px-[10px] py-[5px] rounded-[6px]"
-//                       >
-//                         {tag}
-//                       </span>
-//                     ))}
-//                   </div>
-//                 )}
 //               </div>
 //             </div>
 //           </div>
@@ -92,18 +209,18 @@
 //           {!single && (
 //             <div className=" relative  px-2 lg:px-0 pt-[40px] lg:pt-[211px]  lg:w-[50%]">
 //               <div className=" lg:pl-[58px]">
-//                 <ProjectTitle logo={shopify} title={title2} highlight={highlight2} />
+//                 <ProjectTitle logo={shopify} title={title2} />
 //               </div>
 
 //               <div className=" h-0 lg:h-[28px] flex  items-center">
 //                 <div className="hidden lg:block h-[1px] w-full  bg-white/10"></div>
 //               </div>
-//               <div className="hidden lg:block absolute -left-3 top-[300px]  ">
+//               <div className="hidden lg:block absolute -left-3 top-[300apx]  ">
 //                 <img src={smallicon} alt="icon" />
 //               </div>
 //               <div className=" pt-[16px]  lg:pt-0 lg:pl-[57px]">
-//                 <Link to={`/work/${slug2}`}>
-//                   {renderPreview(idB, leftImg2, centerImg2, rightImg2)}
+//                 <Link to={`/work/${title2?.toLowerCase()}`}>
+//                   <ViewDetail leftImg={leftImg2} centerImg={centerImg2} rightImg={rightImg2} />
 //                 </Link>
 
 //                 <div className="pt-[20px] lg:pt-[24px]">
@@ -117,18 +234,6 @@
 //                       <img key={i} src={img} alt="logo" className="" />
 //                     ))}
 //                   </div>
-//                   {tags2?.length > 0 && (
-//                     <div className="flex flex-wrap gap-[8px] pt-[12px]">
-//                       {tags2.map((tag, i) => (
-//                         <span
-//                           key={i}
-//                           className="text-[11px] lg:text-[12px] leading-[16px] text-[#9DB8FF] bg-[#1B2340] border border-[#3E4A7A]/50 px-[10px] py-[5px] rounded-[6px]"
-//                         >
-//                           {tag}
-//                         </span>
-//                       ))}
-//                     </div>
-//                   )}
 //                 </div>
 //               </div>
 //             </div>
@@ -140,10 +245,12 @@
 // }
 
 // export default WebApp;
+
 import React from "react";
 import shopify from "../../assets/Images/Vector (5).svg";
 import smallicon from "../../assets/Images/smallicon.svg";
 import ViewDetail from "../Homepage/ViewDetail";
+import CardSection from "../Homepage/CardSection";
 import new1 from "../../assets/Images/new1 (1).svg";
 import new2 from "../../assets/Images/new1 (2).svg";
 import new3 from "../../assets/Images/new1 (3).svg";
@@ -152,6 +259,10 @@ import new5 from "../../assets/Images/new1 (5).svg";
 import new6 from "../../assets/Images/new1 (6).svg";
 import ProjectTitle from "./ProjectTitle";
 import { Link } from "react-router-dom";
+
+
+const CARDSECTION_DOUBLE_IDS = [2,4,6]; 
+const CARDSECTION_SINGLE_IDS = [];  
 
 function WebApp({
   single,
@@ -164,11 +275,23 @@ function WebApp({
   title,
   highlight,
   title2,
+  idA, 
+  idB, 
 }) {
   const logos = [
     new2, new3, new1, new5, new4, new6,
     new1, new2, new3, new4, new5, new6,
   ];
+
+  const renderPreview = (id, l, c, r) => {
+    if (CARDSECTION_DOUBLE_IDS.includes(id)) {
+      return <CardSection frontimg={l} backimg={c} frontRotate={8} backRotate={-6} />;
+    }
+    if (CARDSECTION_SINGLE_IDS.includes(id)) {
+      return <CardSection frontimg={c} />;
+    }
+    return <ViewDetail leftImg={l} centerImg={c} rightImg={r} />;
+  };
 
   return (
     <section className="lato-regular">
@@ -188,7 +311,7 @@ function WebApp({
 
             <div className=" pt-[16px] lg:pt-0 lg:pr-[57px]  ">
               <Link to={`/work/${title?.toLowerCase()}`}>
-                <ViewDetail leftImg={leftImg} centerImg={centerImg} rightImg={rightImg} />
+                {renderPreview(idA, leftImg, centerImg, rightImg)}
               </Link>
 
               <div className="pt-[20px] lg:pt-[24px]">
@@ -220,7 +343,7 @@ function WebApp({
               </div>
               <div className=" pt-[16px]  lg:pt-0 lg:pl-[57px]">
                 <Link to={`/work/${title2?.toLowerCase()}`}>
-                  <ViewDetail leftImg={leftImg2} centerImg={centerImg2} rightImg={rightImg2} />
+                  {renderPreview(idB, leftImg2, centerImg2, rightImg2)}
                 </Link>
 
                 <div className="pt-[20px] lg:pt-[24px]">
--- src/Pages/WorkPage.jsx
+++ src/Pages/WorkPage.jsx
@@ -7,9 +7,19 @@ import ServicesSection from '../Components/WorkPage/ServiesSection'
 import first from "../assets/Images/new1.svg";
 import second from "../assets/Images/new2.svg";
 import third from "../assets/Images/new3.svg";
-import fourth from "../assets/Images/new4 (1).svg";
-import fifth from "../assets/Images/new4 (2).svg";
-import sixth from "../assets/Images/new4 (3).svg";
+import restro from "../assets/Images/restro (1).svg"
+import restro1 from "../assets/Images/restro (2).svg"
+import woa1 from "../assets/Images/newwoa (1).svg"
+import woa2 from "../assets/Images/newwoa (2).svg"
+import woa3 from "../assets/Images/newwoa (3).svg"
+import dan1 from "../assets/Images/newdan (1).svg"
+import dan2 from "../assets/Images/newdan (2).svg"
+import apm1 from "../assets/Images/newapm (1).svg"
+import apm2 from "../assets/Images/newapm (2).svg"
+import apm3 from "../assets/Images/newapm (3).svg"
+import lone1 from "../assets/Images/newlone (1).svg"
+import lone2 from "../assets/Images/newlone (2).svg"
+
 function WorkPage() {
   return (
     <>
@@ -18,9 +28,9 @@ function WorkPage() {
     </div>
     <ServicesSection />
   <div className='lg:pb-[20px] work_class px-[20px] md:px-0'>
-     <WebApp  leftImg={first} centerImg={second} rightImg={third}  leftImg2={fourth} centerImg2={fifth} rightImg2={sixth}  title="TryFum"   title2="Syncform"  />
-   <WebApp />
-   <WebApp single  />
+     <WebApp  leftImg={first} centerImg={second} rightImg={third}  leftImg2={restro1} centerImg2={restro}  title="TryFum"   title2="RetroSpec"   idA={1} idB={2} />
+   <WebApp leftImg={woa1} centerImg={woa2} rightImg={woa3}  leftImg2={dan2} centerImg2={dan1}  title="WoahBros"   title2="Doinkrollerz"   idA={3} idB={4} />
+   <WebApp  leftImg={apm1} centerImg={apm2} rightImg={apm3}  leftImg2={lone2} centerImg2={lone1}  title="Apmzee"   title2="Lonepouches"   idA={5} idB={6} />
   </div>
    
 
